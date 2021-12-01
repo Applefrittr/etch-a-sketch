5c0596300faeb43c536e5409de26d440846386dc
applefrittr <jacob.lee.jordan@gmail.com>
2021-11-30 21:14:29 -0500

    Sucessful creation of a 16x16 div grid using js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector('.container')
 
-for (let i=0; i < 256; i++) { 
+for (let i=0; i < 16; i++) { 
     const grid = document.createElement('div')
     grid.classList.add('grid')
-    //grid.textContent = "~" + i + "~"
-    
+      
     container.appendChild(grid)
+
+    for (let j=0; j<16; j++)    {
+        const column = document.createElement('div')
+        column.classList.add('column')
+
+        grid.appendChild(column)
+    }
 }
