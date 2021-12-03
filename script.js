b16edfd920cecad3ae0e58b3f8d898df8c193cb7
applefrittr <jacob.lee.jordan@gmail.com>
2021-12-03 15:35:37 -0500

    comments added. Clear button added.  Various CSS changes

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const slider = document.querySelector('.slider')
 
 pixVal.textContent = slider.value
 
-slider.oninput = function()  {
+slider.oninput = function()  {                           //Slider to determine grid pixels w/ built in grid clear
     pixVal.textContent = this.value
     console.log(slider.value)
     deleteGrid()
     createGrid()
 }
-createGrid()
+createGrid()                                            //creates frist grid on page load
 
-function createGrid()   {
+function createGrid()   {                               //main fucntion which generates grid depending on slider choice
     
     for (let i=0; i < slider.value; i++) { 
         const column = document.createElement('div')
@@ -32,17 +32,8 @@ function createGrid()   {
     const divs = document.querySelectorAll('.grid')
     console.log(divs)
     
-    let draw = false
-
-    let black = false
-    const black = document.querySelector('.black')
-
-    let rainbow = false
-    const rainbow = document.querySelector('.rainbow')
-
-    let erase = false
-    const erase = document.querySelector('.erase')
-    
+    let draw = false                                    // combined eventlisteners moouse down and mouse hover to create the mousedown & hold to draw
+       
     container.addEventListener('mousedown', () =>   {
         draw = true
     })
@@ -52,15 +43,19 @@ function createGrid()   {
     })
     
     divs.forEach((grid)    => {
-        grid.addEventListener('mouseover', () =>    {
-            if(draw == true)    {
-                grid.classList.add('etch')
+        grid.addEventListener('mouseover', () =>    {       //inline css used to 'draw'
+            if(draw == true && blkToggle == true)    {
+                grid.style.backgroundColor = 'black'
+            } else if(draw == true && rnbwToggle == true)   {
+                grid.style.backgroundColor = colors()
+            } else if(draw == true && ersToggle == true)    {
+                grid.style.backgroundColor = 'mintcream'
             }
         })
     })
 }
 
-function deleteGrid()      {
+function deleteGrid()      {                                //clears grid by interating through container and deleting all child nodes
     
     while (container.firstChild)    {
         container.removeChild(container.firstChild)
@@ -68,11 +63,83 @@ function deleteGrid()      {
 }
 
 
-const buttons = document.querySelectorAll('button')
+let blkToggle = false                                       // Logic for Color/Eraser buttons
+const black = document.querySelector('.black')              //Toggle variables used in createGrid() to determine which etch color used via classList.add
 
-buttons.forEach((button)    =>  {
-    button.addEventListener('click', () =>  {
-        button.classList.toggle('selected')
-        
-    })
+let rnbwToggle = false
+const rainbow = document.querySelector('.rainbow')
+
+let ersToggle = false
+const eraser = document.querySelector('.eraser')
+
+const clear = document.querySelector('.clear')
+
+black.addEventListener('click', ()  =>  {
+    blkToggle = true
+    rnbwToggle = false
+    ersToggle = false
+
+    black.classList.add('selected')
+    rainbow.classList.remove('selected')
+    eraser.classList.remove('selected')
+
+}) 
+
+rainbow.addEventListener('click', ()  =>  {
+    blkToggle = false
+    rnbwToggle = true
+    ersToggle = false
+
+    black.classList.remove('selected')
+    rainbow.classList.add('selected')
+    eraser.classList.remove('selected')
+
+}) 
+
+eraser.addEventListener('click', ()  =>  {
+    blkToggle = false
+    rnbwToggle = false
+    ersToggle = true
+
+    black.classList.remove('selected')
+    rainbow.classList.remove('selected')
+    eraser.classList.add('selected')
+
 })
+
+clear.addEventListener('click', () =>   {
+    deleteGrid()
+    createGrid()
+})
+
+function colors()  {                                        //rainbow function, randoms only major colors using rgb.  Only values allowed are 0 or 255
+    let x = Math.floor(Math.random() * 2)
+    let y = Math.floor(Math.random() * 2)
+    let z = Math.floor(Math.random() * 2)
+
+    let r
+    let g
+    let b
+
+    if (x == 0) {
+        r = 0
+    } else {
+        r = 255
+    }
+
+    if (y == 0) {
+        g = 0
+    } else {
+        g = 255
+    }
+
+    if (z == 0) {
+        b = 0
+    } else {
+        b = 255
+    }
+    let color = [r,g,b]
+    console.log('rgb(' + color + ')')
+    return 'rgb(' + color + ')'
+
+}
